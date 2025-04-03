75c3e6c82198aede823dc7490188c95f81abbf91
DarkMatter1217 <prabhjot96439@gmail.com>
2025-04-03 20:37:44 +0530

    Complete

---
+++ Project.py
@@ -0,0 +1,21 @@
+
+from pyexpat import model
+import streamlit as st
+import pickle 
+import helper
+
+model = pickle.load(open('model.pkl','rb'))
+
+st.header("Duplicate Question Pairs")
+
+
+q1=st.text_input("Question 1")
+q2=st.text_input("Question 2")
+
+if st.button("Find"):
+    query=helper.query_point_creator(q1,q2)
+    result=model.predict(query)[0]
+    if result:
+        st.header("The Questions are Duplicate")
+    else:
+        st.header("The Questions are Not Duplicate")
